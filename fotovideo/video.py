8d002e2bb4f49928a36b04479c04f121639edc15
unknown <post-design@mail.ru>
2022-08-24 13:07:31 +0800

    added face recognition

---
+++ fotovideo/video.py
@@ -0,0 +1,30 @@
+#from sre_constants import SUCCESS
+import cv2
+
+face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascades_frontalface_default.xml")
+
+#img = cv2.imread('face.jpg')
+#img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#faces = face_cascades.detectMultiScale(img_gray)
+
+#for (x, y, w, h) in faces:
+#    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 1)
+
+#cv2.imshow('Result', img)
+#cv2.waitKey(0)
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, frame - cap.read()
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascades.detectMultiScale(img_gray)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cv2.imshow('Result', frame)
+    
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
